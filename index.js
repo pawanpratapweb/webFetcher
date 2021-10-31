a13c3eda2fc6356af0cafbc0228dbda9ce86b23e
pawanpratapweb <84120428+pawanpratapweb@users.noreply.github.com>
2021-10-31 13:37:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = process.env.port || 8000;
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log(req.query.url);
   request(req.query.url, function (error, response, body) {
     res.send(body);
   });
